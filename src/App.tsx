8c59a3eb7c1d7d9fd36bcaaa27993a98d34fdb84
Brian Todoroff <brian@todoroff.com>
2024-09-12 23:03:54 -0700

    Import the react-split-pane

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import SplitPane from 'react-split-pane'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+      <SplitPane split="vertical" minSize={300} maxSize={500} defaultSize={300}>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -15,8 +16,8 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+	<h1>Vite + React</h1>
       </div>
-      <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -24,11 +25,11 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
+        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </SplitPane>
   )
 }
 
